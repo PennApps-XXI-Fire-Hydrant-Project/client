35767898fcd8fe92228693947e5fbdf73af1bc97
Davin Lin <lindavin@grinnell.edu>
2020-09-12 15:39:33 -0400

    Setup frontend

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
+import React from "react";
+import { Router, Route, Link } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import { createBrowserHistory as createHistory } from "history";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import ChatRoomPage from "./components/ChatRoomPage";
+const history = createHistory();function App() {  return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router history={history}>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/chatroom" exact component={ChatRoomPage} />
+      </Router>
     </div>
   );
-}
-
-export default App;
+}export default App;
